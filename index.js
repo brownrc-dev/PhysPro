bdba43f7e77374c87227f2515d8ac0c8d7ab5d55
NSReverse <brownrc@live.com>
2018-06-09 20:52:15 -0400

    Automated update and deploy.

--- index.js
+++ index.js
@@ -124,6 +124,10 @@ io.on('connection', function(socket) {
         pushLog('(Client [' + socket.handshake.address + ']) > ' + 'is currently creating a patient profile. \'' + query.text + '\'.')
     });
 
+    socket.on('submitTroubleTicket', function(info) {
+        pushLog('(Client [' + socket.handshake.address + ']) > submitted TC information: ' + info.text)
+    });
+
     socket.on('disconnect', function() {
         pushLog('(PhysPro Server) > Client [' + socket.handshake.address + '] has disconnected.');
     });
